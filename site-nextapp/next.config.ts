8e9776d5993f876485bda63581c6cf19605ba3ca
JDubbs89 <152918687+JDubbs89@users.noreply.github.com>
2025-09-11 00:52:19 -0400

    Configuration and Route Topology

--- site-nextapp/next.config.ts
+++ site-nextapp/next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.imgur.com',
+        port: '',
+        search: ''
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        search: ''
+      }
+    ]
+  }
 };
 
 export default nextConfig;
