b64f80cd14fd612a28eae719a01c64a38399bcd9
JDubbs89 <152918687+JDubbs89@users.noreply.github.com>
2025-09-29 12:01:05 -0400

    Created and tested some new ui classes

--- site-nextapp/src/app/(site)/home/page.tsx
+++ site-nextapp/src/app/(site)/home/page.tsx
@@ -3,6 +3,7 @@ import MediaContainer from "@components/containers/media_container";
 import MediaCarousel from "@components/sections/mediaCarousel/mediaCarousel";
 import SermonHeaderCard from "@components/cards/sermon_header_card";
 import ParagraphCard from "@components/cards/paragraph_card";
+import HeroSection from "@/app/_lib/components/sections/hero_section";
 
 export const metadata = {
   title: "Home",
@@ -21,6 +22,7 @@ export default function Home() {
             ]}/>
           ]}
         />
+        <HeroSection/>
         <section className="paragraph-container">
           <ParagraphCard heading="Welcome!" paragraph ="      Grace Church exists to glorify God by upholding the gospel, making disciples, serving others, and providing community. Located in Jackson County, Michigan, we are committed to living out this mission through corporate worship on Sundays and home-based community groups throughout the week that provide discipleship, care, and mission fulfillment."/>
           <ParagraphCard heading="" paragraph ="      Through the Grace Equip program we intentionally endeavor to train disciples to uphold the gospel in a post-Christian world by holding it high evangelistically while serving our local community through practical outreach and global missions partnerships. Whether you're connecting through our Grace Kids ministry or joining our Evangelism & Missions Team, you'll find a church family dedicated to making disciples who love Jesus, grow with others, and serve the world."/>
---
+++ site-nextapp/src/app/_lib/components/sections/hero_section.tsx
@@ -0,0 +1,16 @@
+
+
+export default function HeroSection(){
+    return (
+        <div className="section-center">
+            <div className="container-2x1">
+            <div className="mx-auto">
+                Hello World 1
+            </div>
+            <div className="mx-auto">
+                Hello World 2
+            </div>
+            </div>
+        </div>
+    )
+}
